24c5c29e2480ddad42b159642a49527cf4f9890f
JavierDominguezGomez <jdomgo3@gmail.com>
2020-08-22 19:47:26 +0200

    Intro JavaScript.

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,17 @@
+// Variables y Constantes
+
+const nombre = "Javier";
+const apellido = "Domínguez";
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+if (true) {
+  let nombre = "John";
+
+  console.log(nombre);
+}
+
+console.log(valorDado);
