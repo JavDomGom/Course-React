d83babae095e4b393733bd83876aaf332916fdd7
JavierDominguezGomez <jdomgo3@gmail.com>
2020-09-26 12:01:17 +0200

    Se añaden test componente AddCategory.

--- 04-gif-expert-app/src/tests/components/AddCategory.test.js
+++ 04-gif-expert-app/src/tests/components/AddCategory.test.js
@@ -1,21 +1,33 @@
 import React from "react";
+import "@testing-library/dom";
 import { shallow } from "enzyme";
 import { AddCategory } from "../../components/AddCategory";
 
 describe("Pruebas en <AddCategory />", () => {
-  const setCategories = () => {};
-  const wrapper = shallow(<AddCategory setCategories={setCategories} />);
+  const setCategories = jest.fn();
+  let wrapper;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    wrapper = shallow(<AddCategory setCategories={setCategories} />);
+  });
 
   test("Debe mostrar el componente correctamente.", () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  test("Debe cambiar la caja de texto. ", () => {
+  test("Debe cambiar la caja de texto.", () => {
     const input = wrapper.find("input");
     const value = "Hola mundo";
 
     input.simulate("change", { target: { value: value } });
 
     expect(wrapper.find("p").text().trim()).toBe(value);
+  });
+
+  test("No debe enviar la info con onSubmit.", () => {
+    wrapper.find("form").simulate("submit", { preventDefault() {} });
+
+    expect(setCategories).not.toHaveBeenCalled();
   });
 });
